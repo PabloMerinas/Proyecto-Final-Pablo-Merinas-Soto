9942bd041911a4ec35c5bdeb09ff3a93f27a3a3e
Pablo Merinas <pablomersot@gmail.com>
2024-03-15 11:39:04 +0100

    Añadido proyecto viajes API

---
+++ src-api/viajes/src/main/java/com/proyecto/viajes/ViajesApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.viajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajesApplication.class, args);
+	}
+
+}
